a8429e415ddf3d523607803894686b9e6008f3a8
Ivan Sharapov <oldrum75@gmail.com>
2022-04-27 22:33:41 +0600

    Все тесты пройдены!

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,7 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
-import { capitalize } from '../src/capitalize';
+import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
